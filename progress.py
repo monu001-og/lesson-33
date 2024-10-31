d9b57d8aa289060e42513701cf5d458e52ad7cea
monu001-og <olatudeogbe@gmail.com>
2024-10-31 06:58:25 +0100

    progress.py

---
+++ progress.py
@@ -0,0 +1,7 @@
+#python progress to print the multiplication taste of 23 from 1 to 10
+#loop from 1 to 10
+for i in range(1,11):
+    #print the multiplacation of 23 by i
+    print(f"23 x {i} = {23 *i}")
+    
+   
